63d6effd047718c201b7120e9b458dfdaaa1a237
B-Kiruthika <927621bit052@mkce.ac.in>
2026-01-12 17:32:04 +0530

    commit message

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "auth-token", "Accept"]
 }));
-
-app.options("*", cors());
+app.options("/*", cors());
 
 // Middleware
 app.use(express.json());
